f5fb76085f81c177d8050d132b6d80d3af7dbb92
DianYueGuo <dianyueguo@gmail.com>
2025-11-21 02:56:23 +0800

    display box2d object

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,14 @@ int main() {
 
     b2BodyDef circleBodyDef = b2DefaultBodyDef();
     circleBodyDef.type = b2_dynamicBody;
-    circleBodyDef.position = (b2Vec2){0.0f, -10.0f};
+    circleBodyDef.position = (b2Vec2){10.0f, 10.0f};
     b2BodyId circleId = b2CreateBody(worldId, &circleBodyDef);
     b2ShapeDef CircleShapeDef = b2DefaultShapeDef();
     CircleShapeDef.density = 1.0f;
     CircleShapeDef.material.friction = 0.3f;
     b2Circle circle;
-    circle.center = (b2Vec2){2.0f, 3.0f};
-    circle.radius = 0.5f;
+    circle.center = (b2Vec2){0.0f, 0.0f};
+    circle.radius = 10.0f;
     b2CreateCircleShape(circleId, &CircleShapeDef, &circle);
 
     float timeStep = 1.0f / 60.0f;
@@ -37,6 +37,13 @@ int main() {
     while (window.isOpen())
     {
         b2World_Step(worldId, timeStep, subStepCount);
+
+        b2Vec2 position = b2Body_GetPosition(circleId);
+        shape.setPosition({position.x, position.y});
+        b2ShapeId shapeId;
+        b2Body_GetShapes(circleId, &shapeId, 1);
+        b2Circle circle = b2Shape_GetCircle(shapeId);
+        shape.setRadius(circle.radius);
 
         while (const auto event = window.pollEvent())
         {
