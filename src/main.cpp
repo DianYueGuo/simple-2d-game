b642d07aaaa6e1521b22fdb4fb88e6fb06f6d095
DianYueGuo <dianyueguo@gmail.com>
2025-11-21 03:27:09 +0800

    add CirclePhysics

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,46 @@
 #include <box2d/box2d.h>
 
 
+class CirclePhysics {
+public:
+    CirclePhysics(CirclePhysics &circlePhysics, b2WorldId &worldId) {
+        b2BodyDef circleBodyDef = b2DefaultBodyDef();
+        circleBodyDef.type = b2_dynamicBody;
+        circleBodyDef.position = (b2Vec2){10.0f, 10.0f};
+
+        circlePhysics.bodyId = b2CreateBody(worldId, &circleBodyDef);
+
+        b2ShapeDef CircleShapeDef = b2DefaultShapeDef();
+        CircleShapeDef.density = 1.0f;
+        CircleShapeDef.material.friction = 0.3f;
+
+        b2Circle circle;
+        circle.center = (b2Vec2){0.0f, 0.0f};
+        circle.radius = 10.0f;
+
+        b2CreateCircleShape(circlePhysics.bodyId, &CircleShapeDef, &circle);
+    }
+
+    b2Vec2 getPosition() {
+        return b2Body_GetPosition(bodyId);
+    }
+
+    float getRadius() {
+        b2ShapeId shapeId;
+        b2Body_GetShapes(bodyId, &shapeId, 1);
+        b2Circle circle = b2Shape_GetCircle(shapeId);
+        return circle.radius;
+    }
+private:
+    b2BodyId bodyId;
+};
+
 int main() {
     b2WorldDef worldDef = b2DefaultWorldDef();
     worldDef.gravity = (b2Vec2){0.0f, 0.0f};
     b2WorldId worldId = b2CreateWorld(&worldDef);
 
-    b2BodyDef circleBodyDef = b2DefaultBodyDef();
-    circleBodyDef.type = b2_dynamicBody;
-    circleBodyDef.position = (b2Vec2){10.0f, 10.0f};
-    b2BodyId circleId = b2CreateBody(worldId, &circleBodyDef);
-    b2ShapeDef CircleShapeDef = b2DefaultShapeDef();
-    CircleShapeDef.density = 1.0f;
-    CircleShapeDef.material.friction = 0.3f;
-    b2Circle circle;
-    circle.center = (b2Vec2){0.0f, 0.0f};
-    circle.radius = 10.0f;
-    b2CreateCircleShape(circleId, &CircleShapeDef, &circle);
+    CirclePhysics circle_physics(circle_physics, worldId);
 
     float timeStep = 1.0f / 60.0f;
     int subStepCount = 4;
@@ -38,12 +62,8 @@ int main() {
     {
         b2World_Step(worldId, timeStep, subStepCount);
 
-        b2Vec2 position = b2Body_GetPosition(circleId);
-        shape.setPosition({position.x, position.y});
-        b2ShapeId shapeId;
-        b2Body_GetShapes(circleId, &shapeId, 1);
-        b2Circle circle = b2Shape_GetCircle(shapeId);
-        shape.setRadius(circle.radius);
+        shape.setPosition({circle_physics.getPosition().x, circle_physics.getPosition().y});
+        shape.setRadius(circle_physics.getRadius());
 
         while (const auto event = window.pollEvent())
         {
