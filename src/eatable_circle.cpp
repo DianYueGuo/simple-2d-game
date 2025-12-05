6539d965e403e964e41b7d476467e0b0006ff98e
DianYueGuo <dianyueguo@gmail.com>
2025-12-05 23:49:00 +0800

    set eatable and toxic color

--- src/eatable_circle.cpp
+++ src/eatable_circle.cpp
@@ -3,6 +3,11 @@
 EatableCircle::EatableCircle(const b2WorldId &worldId, float position_x, float position_y, float radius, float density, float friction, bool toxic) :
     DrawableCircle(worldId, position_x, position_y, radius, density, friction),
     toxic(toxic) {
+    if (toxic) {
+        set_color_rgb(1.0f, 0.0f, 0.0f);
+    } else {
+        set_color_rgb(0.0f, 1.0f, 0.0f);
+    }
 }
 
 void EatableCircle::be_eaten() {
